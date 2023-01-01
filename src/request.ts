cf2d75f657e9478be88a534d5e2f4c1c0e637f5b
wibus-wee <1596355173@qq.com>
2023-01-01 16:00:46 +0800

    fix: request with header and friendly catch

--- src/request.ts
+++ src/request.ts
@@ -1,23 +1,28 @@
 import got, { OptionsOfJSONResponseBody } from "got";
+import { boom } from "./error";
 import { NpmPackage, NPMFiles, GitHubFiles } from "./types";
-
-interface GetOption {
-	header: Record<string, string>;
-}
 
 const header = {
 	"User-Agent":
 		"Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
 };
 
-const get = async <T>(url: string, option?: GetOption): Promise<T> => {
+export const get = async <T>(url: string): Promise<T> => {
 	const initOpt: OptionsOfJSONResponseBody = {
 		retry: {
 			limit: 3,
 		},
-		headers: option?.header,
+		headers: header,
 	};
-	return got.get<T>(url, initOpt).json<T>();
+	const request = await got
+		.get<T>(url, initOpt)
+		.json<T>()
+		.catch((err) => {
+			console.log(err);
+			boom(`Request Failed with ${err.response?.statusCode}`);
+			process.exit(1); // unreachable
+		});
+	return request;
 };
 
 /**
@@ -47,7 +52,7 @@ export function getGitHubFiles(repo: string, dir?: string) {
 	const url =
 		"https://api.github.com" +
 		`/repos/${repo}/contents${dir ? `/${dir}` : ""}`;
-	return get<GitHubFiles[]>(url, { header });
+	return get<GitHubFiles[]>(url);
 }
 
 export async function getGitHubFile(
@@ -56,6 +61,6 @@ export async function getGitHubFile(
 ): Promise<string> {
 	const metaURL =
 		"https://api.github.com" + `/repos/${repo}/contents/${file}`;
-	const meta = await get<{ download_url: string }>(metaURL, { header });
+	const meta = await get<{ download_url: string }>(metaURL);
 	return get(meta.download_url);
 }
